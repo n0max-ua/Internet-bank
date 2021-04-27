25de7c0804183cfd132628c9b9997e7c7ad7964f
n0max <maksym.stepaniuk.it@gmail.com>
2021-04-27 11:15:02 +0300

    init

---
+++ src/test/java/com/ua/IBank/IBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.IBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
